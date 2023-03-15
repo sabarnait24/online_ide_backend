60406e8382af9b5dd59155c3d229b97dfee6b4e7
Sabarna Bhowmik <81832529+sabarnait24@users.noreply.github.com>
2023-03-15 19:47:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const cors = require("cors");
+const { Socket } = require("socket.io");
+require('dotenv').config();
+app.use(cors());
+const path = require('path');
+
+const io = require("socket.io")(http, {
+  cors: {
+
+    origin: ["http://localhost:3000"],
+
+  },
+});
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/", require("./route"));
+
+// app.use("/",require("./socket"))
+const Socketmap = {};
+
+function getAllClients(roomID) {
+  return Array.from(io.sockets.adapter.rooms.get(roomID) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: Socketmap[socketId],
+      };
+    }
+  );
+}
+
+io.on("connection", (Socket) => {
+  Socket.on("join", ({ roomID, username }) => {
+    Socketmap[Socket.id] = username;
+    Socket.join(roomID);
+    const Client = getAllClients(roomID);
+    // console.log(Client);
+    Client.forEach(({ socketId }) => {
+      // console.log(Client);
+      io.to(socketId).emit("NewUserJoin", {
+        Client,
+        username,
+        socketId: Socket.id,
+      });
+    });
+    // io.emit("codesync",{value});
+    
+  });
+  Socket.on("disconnect", () => {
+    var allRooms = Array.from(io.sockets.adapter.rooms || []);
+    // console.log(allRooms);
+    allRooms.forEach((roomID) => {
+      Socket.in(roomID).emit("UserDisconnected", {
+        socketId: Socket.id,
+        username: Socketmap[Socket.id],
+      });
+    });
+    delete Socketmap[Socket.id];
+    Socket.leave();
+  });
+  Socket.on("codechange", ({ roomID, value }) => {
+    io.emit("codesync",{value});
+  });
+  Socket.on("inputchange", ({ roomID, inputvalue }) => {
+    // console.log(inputvalue);
+    io.emit("inputsync",{inputvalue});
+  });
+  Socket.on("outputchange", ({ roomID, outputvalue }) => {
+    // console.log(inputvalue);
+    io.emit("outputsync",{outputvalue});
+  });
+});
+
+if (process.env.NODE_ENV === "production") {
+  console.log("In production stage");
+  app.use(express.static(path.resolve(__dirname,"../", "client", "build")))
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"../", "client", "build", "index.html"));
+  });
+}
+
+http.listen(port, () => {
+  console.log(`connection is successful at ${port}`);
+});
---
+++ route.js
@@ -0,0 +1,110 @@
+// var compiler = require("compilex");
+// var options = { stats: true };
+const express = require("express");
+// compiler.init(options);
+const router = express.Router();
+const fs = require("fs");
+const bodyParser = require("body-parser");
+var request = require("request");
+var jsonParser = bodyParser.json();
+
+router.post("/compile", (req, res) => {
+  const code = req.body.code;
+  const input = req.body.input;
+  const lang = req.body.lang;
+  // try {
+  //   if (lang === "Cpp" || lang === "C") {
+  //     if (!input) {
+  //       var envData = { OS: "windows", cmd: "g++", options: { timeout: 1000 } }; // (uses g++ command to compile )
+
+  //       compiler.compileCPP(envData, code, function (data) {
+  //         console.log(data);
+  //         res.send(data);
+  //         //data.error = error message
+  //         //data.output = output value
+  //       });
+  //     } else {
+  //       var envData = { OS: "windows", cmd: "g++", options: { timeout: 1000 } }; // (uses g++ command to compile )
+
+  //       compiler.compileCPPWithInput(envData, code, input, function (data) {
+  //         console.log(data);
+  //         res.send(data);
+  //       });
+  //     }
+  //   } else if (lang === "Java") {
+  //     if (!input) {
+  //       var envData = { OS: "windows" };
+
+  //       compiler.compileJava(envData, code, function (data) {
+  //         res.send(data);
+  //       });
+  //     } else {
+  //       var envData = { OS: "windows" };
+
+  //       compiler.compileJavaWithInput(envData, code, input, function (data) {
+  //         res.send(data);
+  //       });
+  //     }
+  //   } else if (lang === "Python") {
+  //     if (!input) {
+  //       var envData = { OS: "windows" };
+
+  //       compiler.compilePython(envData, code, function (data) {
+  //         res.send(data);
+  //       });
+  //     } else {
+  //       var envData = { OS: "windows" };
+
+  //       compiler.compilePythonWithInput(envData, code, input, function (data) {
+  //         res.send(data);
+  //       });
+  //     }
+  //   }
+  // } catch (error) {
+  //   res.status(400).send(error);
+  // }
+  var result="";
+  try {
+    var program = {
+      script: code,
+      language: lang,
+      stdin: input,
+      versionIndex: "0",
+      clientId: "b72f395320d93809d7a9fdb2aceaa547",
+      clientSecret:
+        "560fa6567307d002d35e84f65f9ba859d37a24274d337cc4206b3fdd83568eae",
+    };
+    request(
+      {
+        url: "https://api.jdoodle.com/v1/execute",
+        method: "POST",
+        json: program,
+      },
+      function (error, response, body) {
+        // console.log('error:', error);
+        // console.log('statusCode:', response && response.statusCode);
+        // console.log('body:', body);
+        result = body;
+        console.log(body);
+        if (error) {
+          console.log(error);
+        }
+        if(result===""){
+          res.send("error");
+        }
+        res.json(result);
+      }
+    );
+  } catch (error) {
+    console.log("error is" , error);
+  }
+  // const dir = "C:/Users/user/vs code program/web develpoment/ide/server/temp";
+
+  // fs.rm(dir, { recursive: true, force: true }, (err) => {
+  //   if (err) {
+  //     throw err;
+  //   }
+  // });
+});
+
+module.exports = router;
