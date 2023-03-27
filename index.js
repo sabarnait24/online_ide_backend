f4329cb2452ed4b0b5fe7672e2c928efc029380b
Sabarna Bhowmik <81832529+sabarnait24@users.noreply.github.com>
2023-03-27 22:04:21 +0530

    vercel

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const app = express();
 const http = require("http").Server(app);
 const cors = require("cors");
 const { Socket } = require("socket.io");
-require('dotenv').config();
+require("dotenv").config();
 app.use(cors());
-const path = require('path');
+const path = require("path");
 
 const io = require("socket.io")(http, {
   cors: {
-
-    origin: ["https://code-editor-2ss8.onrender.com"],
-
+    origin: ["https://online-ide-frontend.vercel.app/"],
   },
 });
 
@@ -39,6 +37,7 @@ io.on("connection", (Socket) => {
   Socket.on("join", ({ roomID, username }) => {
     Socketmap[Socket.id] = username;
     Socket.join(roomID);
+    console.log(Socketmap);
     const Client = getAllClients(roomID);
     // console.log(Client);
     Client.forEach(({ socketId }) => {
@@ -50,7 +49,6 @@ io.on("connection", (Socket) => {
       });
     });
     // io.emit("codesync",{value});
-    
   });
   Socket.on("disconnect", () => {
     var allRooms = Array.from(io.sockets.adapter.rooms || []);
@@ -62,27 +60,52 @@ io.on("connection", (Socket) => {
       });
     });
     delete Socketmap[Socket.id];
+    console.log(Socketmap);
     Socket.leave();
   });
   Socket.on("codechange", ({ roomID, value }) => {
-    io.emit("codesync",{value});
+    // console.log(roomID,value);
+    // io.in(roomID).emit("codesync", value);
+    // Socket.to(roomID).emit("codesync", value);
+    // io.emit("codesync", { value });
+    // var allRooms = Array.from(io.sockets.adapter.rooms || []);
+    // allRooms.forEach((roomID) => {
+    // console.log(roomID);
+    console.log(Socketmap);
+    const Client = getAllClients(roomID);
+    // console.log(Client);
+    Client.forEach(({ socketId }) => {
+      console.log(socketId, value);
+      io.to(socketId).emit("codesync", {
+        value,
+      });
+    });
+
+    // });
   });
   Socket.on("inputchange", ({ roomID, inputvalue }) => {
-    // console.log(inputvalue);
-    io.emit("inputsync",{inputvalue});
+    var allRooms = Array.from(io.sockets.adapter.rooms || []);
+    allRooms.forEach((roomID) => {
+      Socket.in(roomID).emit("inputsync", {
+        socketId: Socket.id,
+
+        inputvalue,
+      });
+    });
+    // io.to(roomID).emit("inputsync", { inputvalue });
   });
   Socket.on("outputchange", ({ roomID, outputvalue }) => {
     // console.log(inputvalue);
-    io.emit("outputsync",{outputvalue});
+    io.emit("outputsync", { outputvalue });
   });
 });
-if (process.env.NODE_ENV === "production") {
-  console.log("In production stage");
-  app.use(express.static(path.resolve(__dirname,"../", "client", "build")))
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"../", "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   console.log("In production stage");
+//   app.use(express.static(path.resolve(__dirname,"../", "client", "build")))
+//   app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname,"../", "client", "build", "index.html"));
+//   });
+// }
 
 http.listen(port, () => {
   console.log(`connection is successful at ${port}`);
